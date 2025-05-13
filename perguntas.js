d439f5289b66bb48c28ca692089bb4e471365cfe
NicolyAndrade <neves.andrade@escola.pr.gov.br>
2025-05-13 14:28:07 +0000

    ok

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    "Progamação",
+    "O que é HTML?",
+    "linguagem de ,marcação"
+)
+
+criaCartao(
+    "Ingês",
+    "Como é dia do pagamento?",
+    "Pay day"
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
