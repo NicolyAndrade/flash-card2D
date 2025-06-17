e46691c5ba7d74ce9ce925d3c0246e8c899d0366
NicolyAndrade <neves.andrade@escola.pr.gov.br>
2025-06-17 14:28:51 +0000

    ok

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ function criaCartao (categoria, pergunta, resposta) {
                 </div>
 
             </article>
-                       `
-    container.appendChild(cartao)
+            
+            `
+            let respostaEstaVisivel= false 
+            function viraCartao(){
+            respostaEstaVisivel= !respostaEstaVisivel
+            cartao.classList.toggle('active' , respostaEstaVisivel)
+            }
+            cartao.addEventListener('click' ,viraCartao)
+        container.appendChild(cartao)
 }
